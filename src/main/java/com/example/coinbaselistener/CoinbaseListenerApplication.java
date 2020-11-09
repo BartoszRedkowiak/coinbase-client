f45f354c33740700d9e817f00d2f7bb94327c8ab
BartoszRedkowiak <b.redkowiak@gmail.com>
2020-11-09 15:24:12 +0100

    Initial commit

---
+++ src/main/java/com/example/coinbaselistener/CoinbaseListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinbaselistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbaseListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinbaseListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coinbaselistener/CoinbaseListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coinbaselistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinbaseListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
